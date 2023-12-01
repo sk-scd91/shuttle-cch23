b31b13d6f0b53d0d968f6b9c288d376d57672a3f
Sean Deneen <scdeneen@gmail.com>
2023-12-01 18:10:42 -0500

    Day -1: Finished Task 2 Added 500 error.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::status::StatusCode,
+    routing::get,
+    Router
+};
 
 async fn hello_world() -> &'static str {
     "Hello, Santa!"
 }
 
+async fn internal_service_error() -> StatusCode {
+    StatusCode::INTERNAL_SERVER_ERROR
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/", get(hello_world))
+        .route("/-1/error", get(internal_service_error));
 
     Ok(router.into())
 }
