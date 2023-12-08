826e77e8d75f2f63213a2b4369713f1f09934116
Sean Deneen <scdeneen@gmail.com>
2023-12-08 12:29:01 -0500

    Completed Day 8 Part 2
    Also added logic to part 1 to include fixed decimal point.

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -22,10 +22,28 @@ async fn get_pokemon_stats(poke_id: u64) -> reqwest::Result<PokemonStat> {
 // Display Pokemon weight, in kilograms.
 async fn weight(Path(poke_id): Path<u64>) -> Result<String, (StatusCode, String)> {
     get_pokemon_stats(poke_id).await
-        .map(|poke_stat| (poke_stat.weight / 10).to_string())
+        .map(|poke_stat| match (poke_stat.weight / 10, poke_stat.weight % 10) {
+            (weight, 0) => weight.to_string(),
+            (div, md) => format!("{}.{}", div, md)
+        })
+        .map_err(|e| (StatusCode::BAD_GATEWAY, e.to_string()))
+}
+
+async fn drop_pokemon(Path(poke_id): Path<u64>) -> Result<String, (StatusCode, String)> {
+    const GRAVITY: f64 = 9.825;
+    const HEIGHT: f64 = 10.0;
+
+    get_pokemon_stats(poke_id).await
+        .map(|poke_stat| {
+            let weight = poke_stat.weight as f64 * 0.1;
+            let speed = GRAVITY * (2.0 * HEIGHT / GRAVITY).sqrt();
+            let momemtum = weight * speed;
+            momemtum.to_string()
+        })
         .map_err(|e| (StatusCode::BAD_GATEWAY, e.to_string()))
 }
 
 pub fn pokemon_router() -> Router {
     Router::new().route("/weight/:poke_id", get(weight))
+        .route("/drop/:poke_id", get(drop_pokemon))
 }
