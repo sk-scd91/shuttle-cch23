cc81be19027ea7f3654636578c274f304868d839
Sean Deneen <scdeneen@gmail.com>
2023-12-11 14:30:44 -0500

    Fixed Day 7
    Decodes a more universal JSON type and properly handles recipes with 0 values.

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -11,6 +11,8 @@ use axum::{
 use axum_extra::extract::cookie::Cookie;
 use base64::{Engine, prelude::BASE64_STANDARD};
 use serde::{ Serialize, Deserialize };
+use serde_json::Value;
+
 
 #[derive(Deserialize)]
 struct RecipeInput {
@@ -25,15 +27,20 @@ struct RecipeOutput {
 }
 
 fn decode_recipe_cookie(headers: &HeaderMap) -> Result<Vec<u8>, (StatusCode, String)> {
-    let cookie_val = headers.get("cookie")
-        .ok_or((StatusCode::BAD_REQUEST, "Could not find cookies in header".into()))?;
-    let cookie = Cookie::parse(cookie_val.to_str().unwrap())
-        .map_err(|e| (StatusCode::BAD_REQUEST, format!("Unable to parse cookie: {}", e)))?;
+    let cookie_val = headers.get_all("cookie")
+        .iter()
+        .filter_map(|v| match Cookie::parse(v.to_str().unwrap()) {
+            Ok(cookie) => if cookie.name() == "recipe" { Some(Ok(cookie)) } else { None },
+            Err(e) => Some(Err(e))
+        }).next().ok_or((StatusCode::BAD_REQUEST, "Could not find cookies in header".into()))?;
+    
+    let cookie = cookie_val.map_err(|e| (StatusCode::BAD_REQUEST, format!("Unable to parse cookie: {}", e)))?;
+
     BASE64_STANDARD.decode(cookie.value())
         .map_err(|e| (StatusCode::BAD_REQUEST, format!("Unable to decode string: {}", e)))
 }
 
-async fn decode(headers: HeaderMap) -> Result<Json<HashMap<String, u64>>, (StatusCode, String)> {
+async fn decode(headers: HeaderMap) -> Result<Json<Value>, (StatusCode, String)> {
     let encoded_cookie = decode_recipe_cookie(&headers)?;
     let decoded_recipe = serde_json::from_slice(&encoded_cookie)
         .map_err(|e| (StatusCode::BAD_REQUEST, format!("Unable to convert to json: {}", e)))?;
@@ -46,19 +53,21 @@ async fn bake(headers: HeaderMap) -> Result<Json<RecipeOutput>, (StatusCode, Str
         .map_err(|e| (StatusCode::BAD_REQUEST, format!("Unable to convert to json: {}", e)))?;
 
     let cookies = decoded_input.recipe.iter()
+        .filter(|(_, &count)| count > 0)
         .filter_map(|(ingredient, &count)| match decoded_input.pantry.get(ingredient) {
             Some(p) => p.checked_div(count), // Ignore if ingredient has 0 count.
             None => Some(0) // Can't make cookies without ingredient
         }).min().ok_or((StatusCode::BAD_REQUEST, "Could not find all ingredients in pantry".into()))?;
-    let pantry = if cookies == 0 {
-        decoded_input.pantry.clone()
-    } else {
-        decoded_input.pantry.iter()
-            .map(|(ingredient, &count)|(ingredient.to_owned(),
-                count - decoded_input.recipe.get(ingredient).map(|&count| count * cookies)
-                    .unwrap_or(0)))
-            .collect()
-    };
+
+    let mut pantry = decoded_input.pantry.clone();
+    if cookies > 0 {
+        for (ingredient, &count) in decoded_input.recipe.iter() {
+            if let Some(pantry_entry) = pantry.get_mut(ingredient) {
+                *pantry_entry -= cookies * count;
+            }
+        }
+    }
+
     Ok(Json(RecipeOutput { cookies, pantry }))
 }
 
