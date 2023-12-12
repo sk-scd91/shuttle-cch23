42d174f3dcc64fff9557086bb748bc0503538876
Sean Deneen <scdeneen@gmail.com>
2023-12-12 13:56:28 -0500

    Completed Day 12 Part 2

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,14 +1,16 @@
-use axum::{
-    extract::{ Path, State },
-    http::StatusCode,
-    routing::{ get, post },
-    Router,
-};
 use std::{
     collections::BTreeMap,
     sync::{Arc, Mutex},
     time::Instant
 };
+use axum::{
+    extract::{ Json, Path, State },
+    http::StatusCode,
+    routing::{ get, post },
+    Router,
+};
+use ulid::Ulid;
+use uuid::Uuid;
 
 #[derive(Default)]
 struct StringTimes {
@@ -43,10 +45,26 @@ async fn load_time(
     Ok(elapsed.as_secs().to_string())
 }
 
+async fn convert_ulids(Json(ulids): Json<Vec<String>>) -> Result<Json<Vec<String>>, (StatusCode, String)> {
+    let mut uuids = Vec::new();
+
+    for encoded in ulids.iter().rev() {
+        let ulid = Ulid::from_string(encoded)
+            .map_err(
+                |e| (StatusCode::BAD_REQUEST, format!("Unable to parse Ulid: {}", e))
+            )?;
+        let uuid: Uuid = ulid.into();
+        uuids.push(uuid.to_string());
+    }
+
+    Ok(Json(uuids))
+}
+
 pub fn timekeeper_router() -> Router {
     let state = Arc::new(Mutex::new(StringTimes::default()));
     Router::new()
         .route("/save/:s", post(save_time))
         .route("/load/:s", get(load_time))
+        .route("/ulids", post(convert_ulids))
         .with_state(state)
 }
