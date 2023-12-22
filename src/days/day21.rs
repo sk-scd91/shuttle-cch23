92f9ddadfc51c1666c4bed6a8681f75d287f4861
Sean Deneen <scdeneen@gmail.com>
2023-12-22 15:22:26 -0500

    Completed Day 21 Part 2
    Added Photon for remote reverse geocoding library took a while to find one that was both accurate and had reasonable limits for testing.

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -4,49 +4,15 @@ use axum::{
     routing::get,
     Router,
 };
+use photon_geocoding::{
+    filter::ReverseFilter,
+    LatLon,
+    PhotonApiClient
+};
 use s2::{
     cell::Cell,
     cellid::CellID,
 };
-
-// Converts a 64 bit hilbert curve code with a trailing 1 bit to two 32 bit coordinates.
-// See: http://s2geometry.io/devguide/s2cell_hierarchy#hilbert-curve
-/* // Unable to use due to time limit. Keeping here for recognition.
-fn hilbert_to_leaf_coords(hilbert_code: u64) -> (u32, u32) {
-    let mut remaining = hilbert_code & hilbert_code.wrapping_sub(1); //remove last one, if exists.
-    let (mut coord_x, mut coord_y, mut rot, mut flip) = (0u32, 0u32, 0u32, 0u32);
-
-    // Shortcut: Map every two bits to match this pattern
-    // _|_0_|_1_
-    // 0|0,0|1,0
-    // 1|0,1|1,1
-    const EVERY_EVEN_BIT: u64 = 0x5555_5555_5555_5555;
-    remaining ^= (remaining >> 1) & EVERY_EVEN_BIT;
-
-    while remaining != 0 {
-        let quad = (remaining >> (u64::BITS - 2)) as u32;
-
-        // Match odd bits to x and even bits to y, or the other way around if rotated.
-        // Complement the bit if rotated right.
-        coord_x <<= 1;
-        coord_y <<= 1;
-        coord_x |= ((quad >> (rot ^ 1)) ^ flip) & 1;
-        coord_y |= ((quad >> rot) ^ flip) & 1;
-
-        // Rotate on cells 0 or 3, and flip on 3.
-        rot ^= !(quad ^ (quad >> 1)) & 1;
-        flip ^= quad & (quad >> 1) & 1;
-
-        // Shift to the next remaining two bits.
-        remaining <<= 2;
-    }
-
-    // Adjust coordinates.
-    let shift = (hilbert_code.trailing_zeros() as u32 + 1) >> 1;
-    (coord_x.wrapping_shl(shift), coord_y.wrapping_shl(shift))
-}
-
-*/
 
 fn degrees_to_dms(angle: f64, is_latitude: bool) -> (u64, u64, f64, char) {
     // Readjust latitude in case it wraps around the pole.
@@ -87,6 +53,41 @@ async fn get_coords_for_hilbert(Path(code): Path<String>) -> Result<String, (Sta
     )
 }
 
+async fn get_country_from_hilbert(Path(code): Path<String>) -> Result<String, (StatusCode, String)> {
+    let code_num = u64::from_str_radix(&code, 2)
+        .map_err(|e| (StatusCode::BAD_REQUEST, format!("Cannot parse string: {}", e)))?;
+
+    let s2_cell = Cell::from(CellID(code_num));
+    let (lat, lon) = {
+        let point = s2_cell.center();
+        (point.latitude().deg(), point.longitude().deg())
+    };
+
+    tracing::info!("Parsed coordinates: {lat}, {lon}"); // To verify coordinates in other sources.
+
+    // Use https://photon.komoot.io/ for country lookup.
+    // Data provided by https://openstreetmap.org
+    let client = PhotonApiClient::default();
+    let request = tokio::task::spawn_blocking(move || client.reverse_search(
+            LatLon::new(lat, lon), 
+            Some(
+                ReverseFilter::new().language("EN")
+                    .radius(10)
+                    .limit(1)
+            )
+        ).map_err(|e| (StatusCode::BAD_GATEWAY, format!("Unable to process Photon data: {}", e)))
+    ).await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Internal error: {}", e)))??;
+
+    tracing::info!("Found results: {}", request.len());
+
+    if let Some(Some(country)) = request.into_iter().next().map(|f| f.country.clone()) {
+        Ok(country)
+    } else {
+        Err((StatusCode::NOT_FOUND, "Unable to find country data.".into()))
+    }
+}
+
 pub fn world_coord_router() -> Router {
     Router::new().route("/coords/:bin", get(get_coords_for_hilbert))
+        .route("/country/:bin", get(get_country_from_hilbert))
 }
